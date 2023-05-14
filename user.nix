bc33412b1905adb7b465c8a2abbb89e7b861f511
Asish Soudhamma <asishrs@gmail.com>
2023-05-13 22:22:40 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,8 +1,24 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 
-  programs.starship = {
-    enable = true;
-    enableZshIntegration = true;
+  programs = {
+    zsh = {
+      shellAliases = {
+        ls = "exa";
+        l = "exa -lbF --git";
+        ll = "exa -lbGF --git";
+        llm = "exa -lbGd --git --sort=modified";
+        la = "exa -lbhHigUmuSa --time-style=long-iso --git --color-scale";
+        lx = "exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale";
+
+        # specialty views
+        lS = "exa -1";
+        lt = "exa --tree --level=2"; 
+      };
+    }
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
   };
 }
